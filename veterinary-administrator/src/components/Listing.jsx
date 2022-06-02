5be94f46db6407693dc316202a74cdc60f0fd557
listerineh <jsalvarez4@espe.edu.ec>
2022-06-01 23:01:09 -0500

    Update Listing component style

--- veterinary-administrator/src/components/Listing.jsx
+++ veterinary-administrator/src/components/Listing.jsx
@@ -2,22 +2,28 @@ import Patient from "./Patient";
 
 function Listing() {
   return (
-    <div className="md:w-1/2 lg:w-3/5 md:h-screen overflow-y-scroll">
+    <div className="md:w-1/2 lg:w-3/5">
       <h2 className="font-black text-3xl text-center">
         Patient Listing
       </h2>
 
-      <p className="text-xl mt-5 mb-10 text-center">
+      <p className="text-xl mt-5 mb-7 text-center">
         Manage your {''}
         <span className="text-indigo-600 font-bold">
           Patients and Appointments
         </span>
       </p>
 
-      <Patient/>
-      <Patient/>
-      <Patient/>
-      <Patient/>
+      <div className="md:h-screen overflow-y-scroll">
+        <Patient/>
+        <Patient/>
+        <Patient/>
+        <Patient/>
+        <Patient/>
+        <Patient/>
+        <Patient/>
+        <Patient/>
+      </div>
 
     </div>
   );
