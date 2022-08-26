e7b7187024a7f03aa9614c39e533733ab5e41b8c
listerineh <jsalvarez4@espe.edu.ec>
2022-08-26 02:11:38 -0500

    fix: mobile responsive view of the listing

--- src/pages/Listing.jsx
+++ src/pages/Listing.jsx
@@ -2,17 +2,12 @@ import Patient from "../components/Patient";
 
 function Listing({ patients, deletePatient }) {
   return (
-    <div className="md:mx-32 mx-5">
+    <div className="md:mx-32 mx-5 pb-0.5">
       {patients && patients.length ? (
         <>
-          <h2 className="font-black text-4xl text-center mt-10">
+          <h2 className="font-black text-4xl text-center my-10">
             Patient Listing
           </h2>
-
-          <p className="text-xl mt-5 mb-7 text-center">
-            Manage your {""}
-            <span className="text-indigo-600 font-bold">Patients</span>
-          </p>
 
           <div className="grid grid-row-auto gap-4 mb-10">
             {patients.map((patient) => (
