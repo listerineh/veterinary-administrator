4de56d1cda4a2574251ae5422de8a2bd17d939a0
listerineh <jsalvarez4@espe.edu.ec>
2022-08-26 16:54:04 -0500

    fix: form borders

--- src/pages/Form.jsx
+++ src/pages/Form.jsx
@@ -102,13 +102,13 @@ function Form({
   };
 
   return (
-    <div className="md:mx-32 mx-5 pb-0.5 bg-white dark:bg-gray-900 ">
+    <div className="md:mx-32 mx-5 pb-0.5">
       <h2 className="font-black text-4xl text-center my-10 text-black dark:text-white">
         Add <span className="text-indigo-600">patient</span> Information
       </h2>
 
       <form
-        className="shadow-md rounded-lg py-12 px-12 mb-10"
+        className="shadow-md rounded-lg py-12 px-12 mb-10 bg-white dark:bg-gray-900"
         onSubmit={handleSubmit}
       >
         {error && <Error msg="Please fill all the fields!" />}
